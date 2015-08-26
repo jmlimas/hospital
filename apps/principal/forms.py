b6b196d56ed7e5f75deb6fcbb2a67783ad1fb594
juan <jmlimas@gmail.com>
2015-08-26 10:26:20 -0500

    26a

--- apps/principal/forms.py
+++ apps/principal/forms.py
@@ -5,27 +5,25 @@ from models import Alumno
 class AddAlumnoForm(forms.ModelForm):
 	class Meta:
 		model = Alumno
-		exclude = ['ciclo','folio','user',] 
-
-        widgets = {
-                'observacion': forms.Textarea(attrs={'placeholder':'observacion','rows':'4','cols': '40'}),
-                'nombre': forms.TextInput(attrs={'placeholder':'Nombre Alumno','size': '40'}),
-                'escuela': forms.TextInput(attrs={'placeholder':'Nombre de escuela','size': '40'}),
-                'edad': forms.NumberInput(attrs={'placeholder':'Edad','min': '0', 'max': '19'}),
-                'coloniaal': forms.TextInput(attrs={'placeholder':'Colonia alumno','size': '40'}),
-                'callenumal': forms.TextInput(attrs={'placeholder':'Calle y numero alumno','size': '40'}),
-                'localidadal': forms.TextInput(attrs={'placeholder':'Localidades alumno','size': '40'}),
-                'meses': forms.NumberInput(attrs={'placeholder':'Meses','min': '0', 'max': '11'}), 
-                'coloniaesc': forms.TextInput(attrs={'placeholder':'Colonia escuela','size': '40'}),
-                'direccionesc': forms.TextInput(attrs={'placeholder':'Direccion escuela','size': '40'}),
-                'localidadesc': forms.TextInput(attrs={'placeholder':'Localidades escuela','size': '40'}),
-                'grado': forms.NumberInput(attrs={'placeholder': 'Grado','min': '0', 'max':'6'}),              
-                'especialidad': forms.TextInput(attrs={'placeholder':'Especialidad','size': '40'}),
-                'diagnostico': forms.TextInput(attrs={'placeholder':'Diagnostico','size': '40'}),
-                'tema': forms.TextInput(attrs={'placeholder':'Tema','size': '40'}), 
-                'fechaatencion':forms.TextInput(attrs={'placeholder':'aaaa-mm-dd'}), 
-                'horainicio':forms.TextInput(attrs={'placeholder':'hh:mm:ss'}),
-                'horafin':forms.TextInput(attrs={'placeholder':'hh:mm:ss'}),  
-        }   
-
- 
+		exclude = ['ciclo','folio','user'] 
+        observacion = forms.CharField(widget=forms.Textarea(attrs={'rows':'2', 'cols': '40'})) 
+        nombre = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Nombre Alumno','size': '40'}))
+        escuela = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Nombre de escuela','size': '40'}))
+        edad = forms.CharField(widget=forms.NumberInput(attrs={'placeholder': 'Edad','min': '0', 'max': '19'}))
+        coloniaal = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Colonia alumno','size': '40'}))
+        callenumal = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Calle y numero alumno','size': '40'}))
+        localidadal = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Localidades alumno','size': '40'}))
+        meses = forms.CharField(widget=forms.NumberInput(attrs={'placeholder': 'Meses','min': '0', 'max': '11'}))
+        coloniaesc = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Colonia escuela','size': '40'}))
+        direccionesc = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Direccion escuela','size': '40'}))
+        localidadesc = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Localidades escuela','size': '40'}))
+        grado = forms.CharField(widget=forms.NumberInput(attrs={'placeholder': 'Grado','min': '0', 'max':'6'}))
+        especialidad = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Especialidad','size': '40'}))
+        diagnostico = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Diagnostico','size': '40'}))
+        tema = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Tema','size': '40'}))
+        fechaatencion = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'aaaa-mm-dd'}))
+        horainicio = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'hh:mm:ss'}))
+        horafin = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'hh:mm:ss'}))
+       
+                  
+               
